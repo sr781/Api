9e12e64839b09fed018b7a7bc165e1fd7e3f9a5e
Sulav Rai <96390217+sr781@users.noreply.github.com>
2024-02-07 20:14:42 +0000

    Initially created the views for the address data

---
+++ app/views/address_data.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify, request
+from app.controllers.interface import ClientInterface
+from app.database import db
+from app.models.student_data_model import StudentDataModel #Used to find the data for the Address
+from app.models.address_data_model import AddressDataModel
+
+address_data_blueprint = Blueprint("address_data", __name__)
+
+@address_data_blueprint.route("/api/addresses", methods=["GET", "POST"])
+def address_data_list()
