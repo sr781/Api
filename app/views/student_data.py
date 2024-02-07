6b708ff10cf75d5ef1bf0c6cebdf6c3cfb36d23a
Sulav Rai <96390217+sr781@users.noreply.github.com>
2024-02-07 21:18:11 +0000

    finished the 'GET' method for the student data

--- app/views/student_data.py
+++ app/views/student_data.py
@@ -13,19 +13,20 @@ student_data_blueprint = Blueprint("student_data", __name__)
 
 @student_data_blueprint.route("/api/students", methods=["GET", "POST"]) #Where this is located
 def student_data_list():
-    if request.method == "GET":
+    if request.method == "GET": #To recieve all data
         students = db.session.query(StudentDataModel).all() #Gets all the data inside the data model
         if len(students) <= 0:
-            error_msg = "No results"
+            error_msg = "No results" #If there is no data, this message will show up on the return
             return jsonify(msg=error_msg, status=200), 200 #Status 200 code means it is successful
 
         student_schema = StudentSchema(many=True) #copies the schema
         data = student_schema.dump(students) #Stores the data obtained into the schema
 
-        return jsonify(data=data, status=200), 200
+        return jsonify(data=data, status=200), 200 #request was sucessful
     else:
         data = request.json #getting the data from request in json format
         try:
+            #Key value pairs
             name = data["name"]
             nationality = data["nationality"]
             city = data["city"]
@@ -38,17 +39,44 @@ def student_data_list():
             sciences_grade = data["sciences_grade"]
             languages_grade = data["languages_grade"]
 
+            #create a new instance of student, passing on the required fields
             new_student = StudentDataModel(name=name, nationality=nationality, city=city, lat=lat, long=long,
                                            gender=gender, age=age, english_grade=english_grade, maths_grade=maths_grade,
                                            sciences_grade=sciences_grade, languages_grade=languages_grade )
 
-            db.session.add(new_student)
+            db.session.add(new_student) #updates the database with the new student
 
-            db.session.commit()
+            db.session.commit() #This will save the data for the new student
 
             success_message = "New student has been created"
 
-            return jsonify(msg=success_message, data=data, status_code=201), 201
+            return jsonify(msg=success_message, data=data, status_code=201), 201 #Message 201 indicates success and the
+            # creation of a new resource
 
-        except:
-            pass
+        except KeyError: #Error handling. A key error is just there to check if the required columns have their required
+            #values
+            error_message = "Please check to make sure all the data has been added and POST again"
+            return jsonify(msg=error_message, status_code=400) #A 400 Error is due to something on the client side
+
+@student_data_blueprint.route("/api/students/<int: student_id>", methods=["GET"]) #To get one row based on the
+#student id
+def get_single_user(student_id):
+    student = db.session.query(StudentDataModel).filter_by(id=student_id).first() #Looks in the column 'id' for the given
+            # student_id number and selects the first result which is assigned to the variable, "student". Obtains
+            #the data in the row
+
+    if not student_id: #if the table does not find the record with the corresponding id, this will run
+        error_message= f"Student with the id {student_id} was not found "
+        return jsonify(msg=error_message, status=200), 200 #Indicates request was successful (works fine, data was just
+        # not found)
+
+    student_data_schema = StudentSchema(many=False)
+    data = student_data_schema.dump(student) #Result from the first row is taken and added to the schema (does not update)
+
+    return jsonify(data=data, status=200), 200 #Indicates success (not 201 because nothing is changed)
+
+
+
+
+
+
