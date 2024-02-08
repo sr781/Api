0cd74b51ef9488e52e6b9400d7ebf8dde6341ac2
Sulav Rai <96390217+sr781@users.noreply.github.com>
2024-02-08 15:05:56 +0000

    Updated post for address

--- app/views/address_data.py
+++ app/views/address_data.py
@@ -13,10 +13,24 @@ def address_data_list():
     client_interface = ClientInterface("/addresses")
 
     if request.method == "GET":
-        return jsonify(client_interface.get_list())
+        return jsonify(client_interface.get_list()) #Add len checker
     else:
         data = request.json #In the variable data, the request method is used to obtain data (in json format)
         try:
             fk_student_id = data["student_id"] #"student_id" is the foreign key of "id" from the student table
             student = db.session.query(StudentDataModel).filter_by(id=fk_student_id).first() #Obtains first result
+            if not student:
+                error_message = "Student not found"
+                return jsonify(msg=error_message, status=400), 400 #Bad request from client side
+
+            student_id = data["student_id"]
+            number = data["number"]
+            house_name = data["house_name"]
+            road = data["road"]
+            city = data["city"]
+            state = data["state"]
+            country = data["country"]
+            zipcode = data["zipcode"]
+
+            new_address_input = AddressDataModel
 
