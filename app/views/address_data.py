9cfd5c0ea50809c1efafc0a3fb74611b1a993e98
Sulav Rai <96390217+sr781@users.noreply.github.com>
2024-02-08 14:38:03 +0000

    Completed the 'GET' method and partially completed the 'POST' method for the address table

--- app/views/address_data.py
+++ app/views/address_data.py
@@ -4,8 +4,19 @@ from app.database import db
 from app.models.student_data_model import StudentDataModel #Used to find the data for the Address
 from app.models.address_data_model import AddressDataModel
 
-#address_data_blueprint = Blueprint("address_data", __name__)
 
-#@address_data_blueprint.route("/api/addresses", methods=["GET", "POST"])
-#def address_data_list()
+
+address_data_blueprint = Blueprint("address_data", __name__)
+
+@address_data_blueprint.route("/api/addresses", methods=["GET", "POST"])
+def address_data_list():
+    client_interface = ClientInterface("/addresses")
+
+    if request.method == "GET":
+        return jsonify(client_interface.get_list())
+    else:
+        data = request.json #In the variable data, the request method is used to obtain data (in json format)
+        try:
+            fk_student_id = data["student_id"] #"student_id" is the foreign key of "id" from the student table
+            student = db.session.query(StudentDataModel).filter_by(id=fk_student_id).first() #Obtains first result
 
